bb9a91e5c4a7154f8388a8b2e42d630190181ecd
Kalib Hackin <nagui08@hotmail.com>
2019-10-22 00:02:11 -0500

    main title and menu done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,43 @@
 import './style.css'
 
-console.log('Hello World!');
+let mainPage = (()=> {
+  let content = document.getElementById('content');
+  let title = document.createElement('h1');
+  let webMenu = document.createElement('div');
+  let aboutUs = document.createElement('div');
+  let restaurantMenu = document.createElement('div');
+  let location = document.createElement('div');
+  let reservation = document.createElement('div');
+
+  let load = () => {
+    createTitle();
+    createMenu();
+  }
+  
+  let createTitle = () => {
+    title.innerHTML = `Sushi's House`;
+    title.classList.add('main-title');
+    content.appendChild(title);
+  }
+
+  let createMenu = () => {
+    webMenu.classList.add('menu');
+    aboutUs.classList.add('tab');
+    restaurantMenu.classList.add('tab');
+    location.classList.add('tab');
+    reservation.classList.add('tab');
+    aboutUs.innerHTML = 'About Us';
+    restaurantMenu.innerHTML = 'Menu';
+    location.innerHTML = 'Location';
+    reservation.innerHTML = 'Reservation';
+    webMenu.appendChild(aboutUs);
+    webMenu.appendChild(restaurantMenu);
+    webMenu.appendChild(location);
+    webMenu.appendChild(reservation);
+    content.appendChild(webMenu);
+  }
+
+  return {load}
+})();
+
+mainPage.load();
